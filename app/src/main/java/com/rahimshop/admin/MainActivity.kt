663bdc1b6eb8d33e7b7d45141650026868ba2527
mohamedfdjilali-web <mohamedfdjilali@gmail.com>
2026-09-07 12:31:13 +0100

    Update MainActivity.kt

--- app/src/main/java/com/rahimshop/admin/MainActivity.kt
+++ app/src/main/java/com/rahimshop/admin/MainActivity.kt
@@ -4,80 +4,20 @@ import android.app.Activity
 import android.os.Bundle
 import android.graphics.Color
 import android.view.Gravity
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.TextView
 
 class MainActivity : Activity() {
 
-    private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
+        val text = TextView(this)
 
-            // إنشاء WebView
-            webView = WebView(this)
+        text.text = "Rahim Shop\n\nالتطبيق يعمل بنجاح ✅"
+        text.textSize = 24f
+        text.setTextColor(Color.BLACK)
+        text.gravity = Gravity.CENTER
 
-            webView.settings.apply {
-                javaScriptEnabled = true
-                domStorageEnabled = true
-                databaseEnabled = true
-
-                allowFileAccess = false
-                allowContentAccess = false
-            }
-
-            webView.webViewClient = WebViewClient()
-
-            setContentView(webView)
-
-            // فتح لوحة الإدارة
-            webView.loadUrl(
-                "https://shop-dz.gt.tc/admin/login.php"
-            )
-
-        } catch (e: Exception) {
-
-            showError(e)
-
-        } catch (e: Error) {
-
-            showError(e)
-
-        }
-    }
-
-    private fun showError(error: Throwable) {
-
-        val message = TextView(this)
-
-        message.text =
-            "SHOP-DZ\n\n" +
-            "حدث خطأ أثناء تشغيل التطبيق:\n\n" +
-            error.javaClass.name +
-            "\n\n" +
-            (error.message ?: "لا توجد تفاصيل")
-
-        message.textSize = 16f
-        message.setTextColor(Color.BLACK)
-        message.setPadding(40, 60, 40, 60)
-        message.gravity = Gravity.CENTER
-
-        setContentView(message)
-    }
-
-    override fun onBackPressed() {
-
-        if (::webView.isInitialized && webView.canGoBack()) {
-
-            webView.goBack()
-
-        } else {
-
-            super.onBackPressed()
-
-        }
+        setContentView(text)
     }
 }
